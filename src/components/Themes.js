0c989e38556ac5dd586ad40184b31a3a8725d032
aditya4sure <sulphurninja@gmail.com>
2022-05-30 00:22:12 +0530

    first commit

---
+++ src/components/Themes.js
@@ -0,0 +1,19 @@
+// Here we will create dark and light theme variables
+
+export const lightTheme = {
+
+    body:"#f2f3fa",
+    text:"black",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    bodyRgba : "252, 246, 244",
+    textRgba:"0,0,0",
+}
+
+export const DarkTheme = {
+
+    body:"#000000",
+    text:"white",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    textRgba : "252, 246, 244",
+    bodyRgba:"0,0,0",
+}
